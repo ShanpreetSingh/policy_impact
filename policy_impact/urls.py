9c10570f692d441810b645b14741ba69f475d3fa
Shanpreet Singh <sainishanpreet@gmail.com>
2025-05-29 19:23:16 +0530

    Added admin

--- policy_impact/urls.py
+++ policy_impact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import TemplateView
 from schemes import views as scheme_views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),  # Frontend
+    path('admin/', admin.site.urls),
     path('api/schemes/', scheme_views.scheme_list),  # API endpoint
 ]
--- schemes/views.py
+++ schemes/views.py
@@ -1,17 +1,10 @@
-# schemes/views.py
+from django.shortcuts import render
 from django.http import JsonResponse
 
 def home(request):
-    return JsonResponse({
-        "message": "Policy Impact Visualizer API",
-        "endpoints": {
-            "/api/schemes": "List all schemes",
-            "/admin": "Admin panel"
-        }
-    })
+    return render(request, 'index.html')
 
 def scheme_list(request):
-    # Temporary dummy data - we'll connect to DB later
     return JsonResponse({
         "schemes": ["Ujjwala", "PM-KISAN", "MGNREGA"],
         "status": "success"
